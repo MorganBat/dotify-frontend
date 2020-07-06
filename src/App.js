1d81d7f99b7bad0ec477e3e4e687c08349f57743
Morgan <morgan.batterham@gmail.com>
2020-07-06 11:44:31 +1000

    init

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import ViewSongs from './ViewSongs'
+import ViewSong from './ViewSong'
+import {BrowserRouter, Route} from 'react-router-dom'
+import DeleteSong from './DeleteSong'
+import CreateSong from './CreateSong'
+import EditSong from './EditSong'
+
+const App = () => {
+    return (
+        <div>
+            <BrowserRouter>
+                <Route exact path="/" component={ViewSongs} />
+                <Route exact path="/new" component={CreateSong} />
+                <Route exact path="/songs/:id" render={props => <ViewSong songId={props.match.params.id}/>} />
+                <Route exact path="/songs/:id/edit" render={props => <EditSong songId={props.match.params.id}/>} />
+
+            </BrowserRouter>
+
+        </div>
+    )
+}
+
+export default App
